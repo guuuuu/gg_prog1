71f6b6790fcdddd44efe0b3e6f647e8e79622473
guu <kdhmoon@naver.com>
2011-06-21 10:42:09 -0700

    Compute the product from a*1 to a*b where a, b are input

--- gg_prog1.cpp
+++ gg_prog1.cpp
@@ -5,7 +5,11 @@ int main(){
 
 	int a, b;
 	cin >> a >> b;
-	int c = a*b; //product
-	cout << a << " * " << b << " = " << c << endl; //print out the result
+	int c;
+	for(int i=1; i<=b; i++){
+		c = a*i; //product
+		//print out the result
+		cout << a << " * " << i << " = " << c << endl;
+	}
 
 }
